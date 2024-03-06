ce848ad7432af75997eca17421ca421b6c52c2a8
Hugo Lima <hugo.lima@wedsys.com.br>
2024-03-06 08:47:39 -0300

    chore: add match configuration beans

---
+++ api/src/main/java/me/math3ussdl/configuration/MatchConfiguration.java
@@ -0,0 +1,21 @@
+package me.math3ussdl.configuration;
+
+import me.math3ussdl.adapter.MatchJpaAdapter;
+import me.math3ussdl.port.api.MatchServicePort;
+import me.math3ussdl.port.spi.MatchPersistencePort;
+import me.math3ussdl.service.MatchServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MatchConfiguration {
+
+    @Bean
+    public MatchPersistencePort matchPersistencePort() {
+        return new MatchJpaAdapter();
+    }
+
+    public MatchServicePort matchServicePort() {
+        return new MatchServiceImpl(matchPersistencePort());
+    }
+}
