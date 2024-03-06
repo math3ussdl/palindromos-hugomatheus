9278840daa2016e005a15b594d8a020efaa187ef
Hugo Lima <hugo.lima@wedsys.com.br>
2024-03-06 08:41:45 -0300

    chore: add api entrypoint

---
+++ api/src/main/java/me/math3ussdl/PalindromosApplication.java
@@ -0,0 +1,12 @@
+package me.math3ussdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalindromosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalindromosApplication.class, args);
+    }
+}
